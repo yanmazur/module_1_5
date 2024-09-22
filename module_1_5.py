89cbdac4f6824d9364575cd5707c68e6006899a6
yanmazur <zacheper102@gmail.com>
2024-09-22 13:04:11 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = ("home", 2, True)
+print(immutable_var)
+mutable_list = ["book","table","pen"]
+print(mutable_list)
+mutable_list[0:] = [1,2,3]
+print(mutable_list)
+immutable_var[0]=1
